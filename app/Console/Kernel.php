35f599ceb80cb732bd4d6bd8d8acdebabd255f17
bolatovdarkhan25 <bolatovdarkhan25@gmail.com>
2021-06-02 11:43:53 +0600

    fixes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('delete:subscriptions')->everyMinute();
+         $schedule->command('delete:subscriptions')->daily();
     }
 
     /**
